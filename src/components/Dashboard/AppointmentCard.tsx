72eeaeadfd603797ea1da1d59e77fb6a6ce8bf8b
Vladislav <lycakvladislav@gmail.com>
2024-02-29 22:05:30 -0500

    delete test buttons

--- src/components/Dashboard/AppointmentCard.tsx
+++ src/components/Dashboard/AppointmentCard.tsx
@@ -6,27 +6,13 @@ const AppointmentCard: React.FC<IAppointmentCardProps> = ({
   data,
   deleteAppointment,
 }) => {
-  const hash = 'hash';
-  const encryptionIV = 'encryptionIV';
-
   return (
     <div className="grid grid-cols-[1fr_1fr_1.5fr_1fr_1fr_1fr] text-lg mb-4 items-center">
       <p>{data.first_name}</p>
       <p>{data.last_name}</p>
       <p>{data.email}</p>
-      {/* <p>{new Date(data.date).toLocaleDateString()}</p>
-      <p>{data.time}</p> */}
-      {/* TEST BUTTONS */}
-      <Link
-        href={`appointment/${data.id}/canceled?hash=${hash}&iv=${encryptionIV}`}
-      >
-        Test button cancel
-      </Link>
-      <Link
-        href={`appointment/${data.id}/confirmed?hash=${hash}&iv=${encryptionIV}`}
-      >
-        Test button confirm
-      </Link>
+      <p>{new Date(data.date).toLocaleDateString()}</p>
+      <p>{data.time}</p>
       <button onClick={() => deleteAppointment(data.id)} className="cancel_btn">
         Cancel
       </button>
