014e12fbfb443539bb394d7e88de5fba7bc60003
Vladislav <lycakvladislav@gmail.com>
2024-03-05 21:36:51 -0500

    refactor code for cancel and confirm pages

--- src/app/(patientSide)/layout.tsx
+++ src/app/(patientSide)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../../styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import TanstackProvider from "@/components/Providers/TanstackProvider";
 
 export const metadata: Metadata = {
   title: "MedPoint",
@@ -15,17 +16,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        {" "}
-        <body className="relative">
-          <main className="app">
-            <nav className="w-full flex justify-center items-center mb-5 mt-5 ">
-              <span className="text-4xl font-bold text-blue-600 cursor-default">
-                MedPoint
-              </span>
-            </nav>
-            {children}
-          </main>
-        </body>
+        <TanstackProvider>
+          {" "}
+          <body className="relative">
+            <main className="app">
+              <nav className="w-full flex justify-center items-center mb-5 mt-5 ">
+                <span className="text-4xl font-bold text-blue-600 cursor-default">
+                  MedPoint
+                </span>
+              </nav>
+              {children}
+            </main>
+          </body>
+        </TanstackProvider>
       </UserProvider>
     </html>
   );
